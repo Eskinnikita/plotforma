06babf0869fe753b0378c325a229a8d001a98571
Eskin Nikita <ladeuxmaster@gmail.com>
2022-11-01 14:31:43 +0300

    Add db connection

--- server/server.js
+++ server/server.js
@@ -2,13 +2,30 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
 
+const authRouter = require('./routers/authRouter')
+
 require("dotenv").config();
 const port = process.env.PORT;
+const dbUsername = process.env.DB_USERNAME;
+const dbPassword = process.env.DB_PASSWORD;
 const app = express();
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.listen(port, () => {
-  console.log(`Running on port ${port}`)
-})
+app.use("/auth", authRouter)
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${dbUsername}:${dbPassword}@cluster0.k2ge5s8.mongodb.net/?retryWrites=true&w=majority`
+    );
+    app.listen(port, () => {
+      console.log(`Running on port ${port}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+startServer();
