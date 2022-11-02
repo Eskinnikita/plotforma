0f7b224db9c87da82db578fb2810f31eb8bec816
Eskin Nikita <ladeuxmaster@gmail.com>
2022-11-02 14:46:21 +0300

    Add comments to validators

--- server/middlewares/validateRequest.js
+++ server/middlewares/validateRequest.js
@@ -1,5 +1,6 @@
 const {validationResult} = require('express-validator')
 
+//Collects all errors to response
 module.exports = function validateRequest(req, res, next) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -1,7 +1,6 @@
 const Router = require("express");
 const router = new Router();
 const controller = require("../controllers/authController");
-const { check } = require("express-validator");
 const authMiddleware = require("../middlewares/authMiddleware");
 const roleMiddleware = require("../middlewares/roleMiddleware");
 const validateRequest = require("../middlewares/validateRequest");
